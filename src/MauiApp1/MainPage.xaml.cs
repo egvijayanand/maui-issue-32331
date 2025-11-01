a74fe42f58aac221c5de100fe0653b60db1e476d
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2025-11-01 12:50:20 +0530

    Sample project to simulate the issue https://github.com/dotnet/maui/issues/32331

---
+++ src/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnNavigateClicked(object? sender, EventArgs e)
+		=> await Shell.Current.GoToAsync(nameof(SearchPage));
+}
---
+++ src/MauiApp1/SearchPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiApp1;
+
+public partial class SearchPage : ContentPage
+{
+	public SearchPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnBackClicked(object sender, EventArgs e)
+		=> await Shell.Current.GoToAsync("..");
+}
